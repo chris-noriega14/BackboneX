3fcf597704c7c43ffa3cf376af85f2f2286dab47
chris-noriega14 <chris.noriega14@hotmail.com>
2021-11-24 14:50:05 -0600

    Updated server, comment out db

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
+// const { ApolloServer } = require('apollo-server-express');
+// const path = require('path');
 
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+// const { typeDefs, resolvers } = require('./schemas');
+// const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
 
-server.applyMiddleware({ app });
+// server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -26,9 +26,9 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-db.once('open', () => {
+// db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    // console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
-});
+// });

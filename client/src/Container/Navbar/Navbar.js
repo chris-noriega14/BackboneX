0cc0dc8206f054a3b48eae8b03e1e05af10f7cc1
Faizila Amlani <amlani.faizila@gmail.com>
2021-12-01 11:40:53 -0600

    added popper and bootstrap script

--- client/src/Container/Navbar/Navbar.js
+++ client/src/Container/Navbar/Navbar.js
@@ -30,28 +30,7 @@ function Nav() {
 
            <ul>
           <li><a className="nav-link scrollto active" href="/dashboard">Home</a></li>
-          <li><a className="nav-link scrollto " href="/exercise">Exercises</a></li>
-          <li><a className="nav-link scrollto" href="/exercise/list">My List</a></li>
-        <li className="dropdown"><a href="#"><span>Resources</span> <i className="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li className="dropdown"><a href="#"><span>Deep Drop Down</span> <i className="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-            </li>
-          <li><a className="nav-link scrollto" href="#contact">Contact</a></li>
-     
-          <li><a href="/users/logout" className="nav-link" id="logout">Logout</a></li>
+        <li><a className="nav-link scrollto" href="#contact">Contact</a></li>
         </ul>
  </nav>
 

c897732b75335e89e3a881029c52c591a7adf8b3
chris-noriega14 <chris.noriega14@hotmail.com>
2021-12-03 14:33:47 -0600

    Added ball,band,stretch exercise images to application

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,12 +4,15 @@ const typeDefs = gql`
 type Exercise {
   _id: ID
   exerciseName: String
+  exercisePath: String
   imgStart: String
   imgEnd:String
 }
 
 type Query {
     exercises (type: String): [Exercise]
+    bandExercises (type: String): [Exercise]
+    stretchExercises (type: String): [Exercise]
   }
 `;
 

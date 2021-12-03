6a81ae5e8a4bfe2f1635507478330246291e7aac
Faizila Amlani <amlani.faizila@gmail.com>
2021-12-03 11:27:29 -0600

    added condition and mylist.js

---
+++ client/src/Pages/Exercise/Mylist.js
@@ -0,0 +1,22 @@
+import './Exercises.css';
+
+function Mylist() {
+  return (
+      <div>
+<section id="exercises" class="exercises">
+      <div class="container">
+
+        <div class="section-title">
+          <h1>My Exercise List</h1>
+          
+     {/* Card */}
+
+        </div>
+
+      </div>
+    </section>
+    </div>
+  );
+}
+
+export default Mylist;

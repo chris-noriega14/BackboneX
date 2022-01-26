8b4513243406f75350bfbc8122e523c8ceaf6701
chris-noriega14 <chris.noriega14@hotmail.com>
2022-01-26 11:05:56 -0600

    Added styling to admin page

--- client/src/Pages/Exercise/Admin.js
+++ client/src/Pages/Exercise/Admin.js
@@ -70,7 +70,25 @@ const Admin = () => {
 
         <div className="section-title">
         <h1>Videos</h1>
-          
+        <div>Step 1: Navigate to desired Youtube video and right click</div>
+        <div>Step 2: Copy embedded video from Youtube</div>
+        <div>Step 3: Paste the embedded code to Youtube URL</div>
+        <br></br>
+        <div>
+          <div className="d-flex flex-column col-8">
+      <TextField onChange={handleInputChange} id="outlined-basic" name='exerciseUrl' label="Video Title" variant="outlined" />
+      <br></br>
+      <TextField
+          id="outlined-multiline-static"
+          label="YouTube URL"
+          name='exerciseName'
+          multiline
+          rows={4}
+        />
+          </div>
+      <br></br>
+      <Button onClick={handleFormSubmit} variant="contained">Add Video to database</Button>
+      </div>  
      {/* Card */}
      <div>
 
@@ -84,12 +102,7 @@ const Admin = () => {
         </div>
 
       </div>
-      <div>
-      <TextField onChange={handleInputChange} id="outlined-basic" name='exerciseName' label="YouTube URL" variant="outlined" />
-      <TextField onChange={handleInputChange} id="outlined-basic" name='exerciseUrl' label="Video Title" variant="outlined" />
-
-<Button onClick={handleFormSubmit} variant="contained">Add Video to database</Button>
-      </div>
+      
     </section>
     ) : (<Redirect to="/" />)}
     </div>
